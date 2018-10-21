2c4518bccfaad22a1eeb2dff1decf4a72a43a594
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2018-10-21 02:53:41 -0400

    added coordinate movements

--- src/main/java/piece/Piece.java
+++ src/main/java/piece/Piece.java
@@ -1,6 +1,7 @@
 package piece;
 
 import board.Board;
+import board.Coordinate;
 import board.Square;
 import chess.Color;
 
@@ -35,6 +36,50 @@ public abstract class Piece {
     protected void goTo(Square square) {
         this.square.removePiece();
         square.setPiece(this);
+    }
+
+    /**
+     * @param n Number of spaces forward
+     * @return Y coordinate of this piece if it were to go forward n spaces
+     */
+    protected int yForward(int n) {
+        Coordinate start = this.square.getCoordinate();
+        boolean white = this.color == Color.White;
+        int forward = start.getY() + (white ? 1 : -1);
+        return forward * n;
+    }
+
+    /**
+     * @param n Number of spaces to go back
+     * @return Y coordinate of this piece if it were to go back n spaces
+     */
+    protected int yBack(int n) {
+        Coordinate start = this.square.getCoordinate();
+        boolean white = this.color == Color.White;
+        int back = start.getY() + (white ? -1 : 1);
+        return back * n;
+    }
+
+    /**
+     * @param n Number of spaces to go right
+     * @return X coordinate of this piece if it were to go right n spaces
+     */
+    protected int xRight(int n) {
+        Coordinate start = this.square.getCoordinate();
+        boolean white = this.color == Color.White;
+        int right = start.getX() + (white ? 1 : -1);
+        return right * n;
+    }
+
+    /**
+     * @param n Number of spaces to go left
+     * @return X coordinate of this piece if it were to go left n spaces
+     */
+    protected int xLeft(int n) {
+        Coordinate start = this.square.getCoordinate();
+        boolean white = this.color == Color.White;
+        int left = start.getX() + (white ? -1 : 1);
+        return left * n;
     }
 
     /**
