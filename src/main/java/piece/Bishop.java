b596927f52e66f4da6aacc33dbede1a7a6e1ca68
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2018-10-24 14:35:01 -0400

    created bishop

---
+++ src/main/java/piece/Bishop.java
@@ -0,0 +1,52 @@
+package piece;
+
+import board.Board;
+import board.Coordinate;
+import board.Square;
+import chess.Color;
+
+import java.util.List;
+
+/**
+ * Represents a bishop
+ * @author Ammaar Muhammad Iqbal
+ */
+public class Bishop extends Piece {
+
+    /**
+     * Creates a new bishop
+     * @param board Board that the bishop is on
+     * @param square Square that the bishop is on
+     * @param color Color of the bishop
+     */
+    public Bishop(Board board, Square square, Color color) {
+        super(board, square, color, 4);
+    }
+
+    @Override
+    protected Coordinate nextCoordinate(int pathNum, int stepNum, Coordinate c) {
+        if (pathNum == 1) return new Coordinate(this.xRight(stepNum), this.yForward(stepNum));
+        else if (pathNum == 2) return new Coordinate(this.xRight(stepNum), this.yBack(stepNum));
+        else if (pathNum == 3) return new Coordinate(this.xLeft(stepNum), this.yBack(stepNum));
+        else return new Coordinate(this.xLeft(stepNum), this.yForward(stepNum));
+    }
+
+    @Override
+    protected boolean stepCondition(int pathNum, int stepNum, Coordinate c) {
+        return true;
+    }
+
+    @Override
+    protected void checkCanMove(boolean found, int pathNum, List<Square> path) throws Exception {
+        if (!found) throw new Exception("A bishop can't go there");
+        if (!Board.isPathClear(path)) throw new Exception("The bishop's path is blocked");
+    }
+
+    /**
+     * @return ASCII representation of the bishop
+     */
+    @Override
+    public String toString() {
+        return super.toString() + "B";
+    }
+}
