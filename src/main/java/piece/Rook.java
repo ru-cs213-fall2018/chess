29ba04067197bbde8c78b51cdae591a96b1fb6c6
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2018-10-24 02:07:14 -0400

    made it easier to move pieces

--- src/main/java/piece/Rook.java
+++ src/main/java/piece/Rook.java
@@ -5,7 +5,7 @@ import board.Coordinate;
 import board.Square;
 import chess.Color;
 
-import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Represents a rook
@@ -20,50 +20,26 @@ public class Rook extends Piece {
      * @param color Color of the rook
      */
     public Rook(Board board, Square square, Color color) {
-        super(board, square, color);
+        super(board, square, color, 4);
     }
 
     @Override
-    public void moveTo(Square square) throws Exception {
+    protected Coordinate nextCoordinate(int pathNum, int stepNum, Coordinate c) {
+        if (pathNum == 1) return new Coordinate(c.getX(), this.yForward(stepNum));
+        else if (pathNum == 2) return new Coordinate(c.getX(), this.yBack(stepNum));
+        else if (pathNum == 3) return new Coordinate(this.xRight(stepNum), c.getY());
+        else return new Coordinate(this.xLeft(stepNum), c.getY());
+    }
 
-        // Initialize path and found
-        ArrayList<Square> path = new ArrayList<>();
-        boolean found = false;
+    @Override
+    protected boolean stepCondition(int pathNum, int stepNum, Coordinate c) {
+        return true;
+    }
 
-        // Run 4 times for front, back, right, left
-        out: for (int j = 0; j < 4; j++) {
-
-            // Clear the path becuase each direction is alone
-            path.clear();
-            Coordinate c = this.square.getCoordinate();
-
-            // Look in direction as long as the coordinate is in the board
-            for (int i = 1; Board.isInBoard(c); i++) {
-
-                // Add corresponding square to the path
-                Square s = this.board.getSquare(c);
-                path.add(s);
-
-                // If the square is the destination break and set found
-                if (square.equals(s)) {
-                    found = true;
-                    break out;
-                }
-
-                // Updated the coordinate based on the out's j
-                if (j == 0) c = new Coordinate(c.getX(), this.yForward(i));
-                else if (j == 1) c = new Coordinate(c.getX(), this.yBack(i));
-                else if (j == 2) c = new Coordinate(this.xRight(i), c.getY());
-                else c = new Coordinate(this.xLeft(i), c.getY());
-            }
-        }
-
-        // Check if found and path is clear
+    @Override
+    protected void checkCanMove(boolean found, int pathNum, List<Square> path) throws Exception {
         if (!found) throw new Exception("A rook can't move there");
         if (!Board.isPathClear(path)) throw new Exception("The rook's path is blocked");
-
-        // Move the piece
-        this.goTo(square);
     }
 
     /**
